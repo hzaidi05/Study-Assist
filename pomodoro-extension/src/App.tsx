534cf5bcc6fccbbd672c7ff7e8b5fb72a9a4898c
Hamza Zaidi <cookiestein3@gmail.com>
2024-01-21 11:46:47 -0500

    Update code to adhere to CSP, add timer functionality

--- pomodoro-extension/src/App.tsx
+++ pomodoro-extension/src/App.tsx
@@ -12,10 +12,58 @@ function App() {
     longBreakInterval: 4,
   };
 
+  let interval;
+
+  const mainButton = document.getElementById('js-btn');
+  mainButton.addEventListener('click', () => {
+    const { action } = mainButton.dataset;
+    if (action === 'start') {
+      startTimer();
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    switchMode('pomodoro');
+  });
+
   const modeButtons = document.querySelector('#js-mode-buttons');
   if (modeButtons) {
     modeButtons.addEventListener('click', handleMode);
 
+    function getRemainingTime(endTime) {
+      const currentTime = Date.parse(new Date());
+      const difference = endTime - currentTime;
+    
+      const total = Number.parseInt(difference / 1000, 10);
+      const minutes = Number.parseInt((total / 60) % 60, 10);
+      const seconds = Number.parseInt(total % 60, 10);
+    
+      return {
+        total,
+        minutes,
+        seconds,
+      };
+    }
+    
+    function startTimer() {
+      let { total } = timer.remainingTime;
+      const endTime = Date.parse(new Date()) + total * 1000;
+    
+      mainButton.dataset.action = 'stop';
+      mainButton.textContent = 'stop';
+      mainButton.classList.add('active');
+
+      interval = setInterval(function() {
+        timer.remainingTime = getRemainingTime(endTime);
+        updateClock();
+    
+        total = timer.remainingTime.total;
+        if (total <= 0) {
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
+    
     function updateClock() {
       const { remainingTime } = timer;
       const minutes = \`\${remainingTime.minutes}\`.padStart(2, '0');
@@ -115,12 +163,6 @@ function App() {
           </button>
         </div>
       </main>
-      <div className="hidden">
-        <audio src="backtowork.mp3" data-sound="pomodoro"></audio>
-        <audio src="break.mp3" data-sound="shortBreak"></audio>
-        <audio src="break.mp3" data-sound="longBreak"></audio>
-      </div>
-      <script src="index.tsx"></script>
     </body>
     </div>
   );
